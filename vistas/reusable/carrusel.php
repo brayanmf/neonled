5146ca162061ed6f7b411a1ca9d31785ad76a93f
mr01robot <42098976+mr01robot@users.noreply.github.com>
2021-05-29 23:06:21 -0500

    carrusel

--- vistas/reusable/carrusel.php
+++ vistas/reusable/carrusel.php
@@ -2,14 +2,15 @@
     <div class="hi-prev "></div>
     <div class="hi-next "></div>
     <ul>
-        <li><img src="../public/images/tendencia/boca_lengua.png" alt="Img 1" /></li>
-        <li><img src="../public/images/fondos/fondo_s.jpg" alt="Img 2" /></li>
-        <li><img src="../public/images/fondos/fondo_s.jpg" alt="Img 3" /></li>
-        <li><img src="../public/images/fondos/fondo_s.jpg" alt="Img 4" /></li>
-        <li><img src="../public/images/fondos/fondo_s.jpg" alt="Img 5" /></li>
-        <li><img src="../public/images/fondos/fondo_s.jpg" alt="Img 6" /></li>
-        <li><img src="../public/images/fondos/fondo_s.jpg" alt="Img 7" /></li>
-        <li><img src="../public/images/fondos/fondo_s.jpg" alt="Img 7" /></li>
+      <!--  <li><img src="../public/images/tendencia/boca_lengua.png" alt="Img 1" /></li>-->
+        <li><img src="./public/images/tendencia/boca_lengua.png" alt="Img 1" /></li>
+        <li><img src="./public/images/fondos/fondo_s.jpg" alt="Img 2" /></li>
+        <li><img src="./public/images/fondos/fondo_s.jpg" alt="Img 3" /></li>
+        <li><img src="./public/images/fondos/fondo_s.jpg" alt="Img 4" /></li>
+        <li><img src="./public/images/fondos/fondo_s.jpg" alt="Img 5" /></li>
+        <li><img src="./public/images/fondos/fondo_s.jpg" alt="Img 6" /></li>
+        <li><img src="./public/images/fondos/fondo_s.jpg" alt="Img 7" /></li>
+        <li><img src="./public/images/fondos/fondo_s.jpg" alt="Img 7" /></li>
     </ul>
     <div class="titulo-slide">
         <h1>¿Aún no tienes el tuyo?</h1>
